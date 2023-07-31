a0e53d426a8152404de5f9bdec89bdc614bcda11
Hanieh4350 <hanieh4350@gmail.com>
2023-07-31 17:53:14 +0330

    implement exception handling

--- src/main/java/com/example/customerinformationsystem/error/NoCustomersToSaveException.java
+++ src/main/java/com/example/customerinformationsystem/error/NoCustomersToSaveException.java
@@ -2,23 +2,7 @@ package com.example.customerinformationsystem.error;
 
 public class NoCustomersToSaveException extends RuntimeException {
 
-    public NoCustomersToSaveException() {
-        super();
-    }
-
     public NoCustomersToSaveException(String message) {
         super(message);
-    }
-
-    public NoCustomersToSaveException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NoCustomersToSaveException(Throwable cause) {
-        super(cause);
-    }
-
-    protected NoCustomersToSaveException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/com/example/customerinformationsystem/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/customerinformationsystem/error/RestResponseEntityExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.example.customerinformationsystem.error;
 
 import com.example.customerinformationsystem.entity.ErrorMessage;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 
 @ControllerAdvice
